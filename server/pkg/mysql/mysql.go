d5db60904854b3386ea85a66cf9d052e41ba9efb
micodee <martomhidayat>
2023-03-25 01:21:37 +0700

    migrasi postgres

--- server/pkg/mysql/mysql.go
+++ server/pkg/mysql/mysql.go
@@ -2,17 +2,24 @@ package mysql
 
 import (
 	"fmt"
+	"os"
 
-	"gorm.io/driver/mysql"
+	"gorm.io/driver/postgres"
 	"gorm.io/gorm"
 )
 
 var ConnDB *gorm.DB
 
 func DatabaseInit() {
+	var DB_HOST = os.Getenv("DB_HOST")
+	var DB_USER = os.Getenv("DB_USER")
+	var DB_PASSWORD = os.Getenv("DB_PASSWORD")
+	var DB_NAME = os.Getenv("DB_NAME")
+	var DB_PORT = os.Getenv("DB_PORT")
+	
 	var err error
-	DBurl := "admin:admin@tcp(localhost:3306)/dumbsound?charset=utf8mb4&parseTime=True&loc=Local"
-	ConnDB, err = gorm.Open(mysql.Open(DBurl), &gorm.Config{})
+	DBurl := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s", DB_HOST, DB_USER, DB_PASSWORD, DB_NAME, DB_PORT)
+	ConnDB, err = gorm.Open(postgres.Open(DBurl), &gorm.Config{})
 
 	if err != nil {
 		panic(err)
